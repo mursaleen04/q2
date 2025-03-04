b21422ca40328234f32bc7ec0967ca26a029edcb
mursaleen <mursaleenarshad65@gmail.com>
2025-03-04 13:27:55 +0500

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2
+{
+ 
+  
+class Employee
+    {
+        public int EmployeeID;
+        public string Name;
+        public string Department;
+        public double Salary;
+
+        public Employee(int id, string name, string department, double salary)
+        {
+            EmployeeID = id;
+            Name = name;
+            Department = department;
+            Salary = salary;
+        }
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine($"Employee ID = {EmployeeID}");
+            Console.WriteLine($"Name = {Name}");
+            Console.WriteLine($"Department = {Department}");
+            Console.WriteLine($"Salary = {Salary} RUPEES");
+            Console.WriteLine("---------------------------------");
+        }
+
+        public void IncreaseSalary(double amount)
+        {
+            Salary += amount;
+        }
+    }
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+     
+            {
+                Employee emp1 = new Employee(2772, "ali", "finance", 55000);
+                emp1.IncreaseSalary(6000);
+                emp1.DisplayEmployeeDetails();
+
+                Employee emp2 = new Employee(5665, "tahir", "marketing", 62000);
+                emp2.IncreaseSalary(8000);
+                emp2.DisplayEmployeeDetails();
+
+                Employee emp3 = new Employee(7878, "usman", "sale and purchase", 72000);
+                emp3.IncreaseSalary(4000);
+                emp3.DisplayEmployeeDetails();
+            }
+        }
+
+    }
+}
